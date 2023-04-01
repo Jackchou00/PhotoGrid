460deddba93754db7f0e1ca6f9d8be0b9ddf7ddc
Jackchou <Jackchou00@Outlook.com>
2023-04-01 15:05:12 +0800

    Adjust UI

--- ui.py
+++ ui.py
@@ -8,20 +8,35 @@ class MyWindow(QWidget):
         super().__init__()
         self.initUI()
 
-    def initUI(self):  
+    def initUI(self): 
+        self.setWindowTitle('PhotoGrid') 
+
         # 添加控件
         self.path_edit = QLineEdit(self)
         self.path_btn = QPushButton('选择文件夹', self)
         self.run_btn = QPushButton('运行程序', self)
+        self.quit_btn = QPushButton('退出程序', self)
 
         # 设置控件位置
         self.path_edit.setGeometry(10, 10, 200, 30)
         self.path_btn.setGeometry(220, 10, 100, 30)
         self.run_btn.setGeometry(10, 50, 100, 30)
+        self.quit_btn.setGeometry(120, 50, 100, 30)
 
-        # 连接函数
+        # 连接信号与槽
         self.path_btn.clicked.connect(self.show_file_dialog)
         self.run_btn.clicked.connect(self.run_program)
+        self.quit_btn.clicked.connect(QApplication.instance().quit)
+
+        self.center()
+        self.show()
+
+    def center(self):
+        """ 窗口居中显示 """
+        screen = self.frameGeometry()
+        center_point = self.screen().availableGeometry().center()
+        screen.moveCenter(center_point)
+        self.move(screen.topLeft())
 
     def show_file_dialog(self):
         """ 显示文件选择器 """
@@ -42,5 +57,4 @@ class MyWindow(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MyWindow()
-    window.show()
     sys.exit(app.exec())
